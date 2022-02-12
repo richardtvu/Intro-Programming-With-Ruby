4c0de67435da3fd93c6f01f0a4b7f6aca0f96e94
Richard Vu <83021d9e@slack.launchschool.com>
2022-02-12 07:51:39 -0500

    Add some comments explaining what the code is doing and make use of escape characters.

--- basics/1-concatenation.rb
+++ basics/1-concatenation.rb
@@ -1,10 +1,13 @@
-# The first exercise for the basics chapter.
+# The first exercise (enhanced) for the basics chapter.
+#
+
 puts "Please enter your first name."
-first = gets.chomp
-first[0] = first[0].upcase
+first = gets.chomp # The .chomp is needed to remove the extra line that comes from getting user input.
+first[0] = first[0].upcase # the [0] means access the first character of the string. Upcase will capitalize all input, so I wanted to isolate capitalization to the first character.
+
 puts "Please enter your last name."
 last = gets.chomp
 last[0] = last[0].upcase
 
 puts "Using the + , your first and last name are: " + first + " " + last
-puts "Using string interpolation, your first name is #{first} and your last name is #{last}"
+puts "Using string interpolation, i.e. \`\#\{something\}\`: your first name is #{first} and your last name is #{last}"
