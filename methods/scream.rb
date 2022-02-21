8bdc947a50084a68d3d6f52d21b631125a83f360
Richard Vu <83021d9e@slack.launchschool.com>
2022-02-21 18:09:52 -0500

    Add the scream file for EXERCISES4 and 5 of the methods chapter.

---
+++ methods/scream.rb
@@ -0,0 +1,13 @@
+# Exercises 4 and 5
+def scream(words)
+  words = words + "!!!!"
+  return
+  puts word
+end
+
+scream("Yippee") # will return nil and printed nothing.
+
+def scream4(words)
+  words = words + "!!!!"
+  puts words # this will print the words with!!!!! at the end to the screen, but will return nil
+end
